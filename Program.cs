50387c3a8e6de88710a34762cd1fe06d78fba1ef
cheolwook <loky123@naver.com>
2022-05-19 09:32:07 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interface_study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
